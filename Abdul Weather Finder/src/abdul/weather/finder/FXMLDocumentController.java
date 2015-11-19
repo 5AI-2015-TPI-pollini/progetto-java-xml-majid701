e1497c2c4a26538c31cd810973aa2d9ef6009655
majid <majid@majid-Laptop>
2015-11-19 12:50:41 +0100

    Project created

---
+++ Abdul Weather Finder/src/abdul/weather/finder/FXMLDocumentController.java
@@ -0,0 +1,28 @@
+package abdul.weather.finder;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Abdul Majid
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
