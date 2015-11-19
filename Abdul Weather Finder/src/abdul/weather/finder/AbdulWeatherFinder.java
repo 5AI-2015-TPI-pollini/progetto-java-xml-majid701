af637213cd7a595d12d685d6e48eb6cdea561040
majid <majid@majid-Laptop>
2015-11-19 12:54:41 +0100

    commento inutile

--- Abdul Weather Finder/src/abdul/weather/finder/AbdulWeatherFinder.java
+++ Abdul Weather Finder/src/abdul/weather/finder/AbdulWeatherFinder.java
@@ -15,7 +15,7 @@ public class AbdulWeatherFinder extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+        //comment
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
