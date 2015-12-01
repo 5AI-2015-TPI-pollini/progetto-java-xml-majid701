628fba65fbf6d0ee669fb37f7b24c2425cfd7527
majid <majid@majid-Laptop>
2015-12-01 21:36:35 +0100

    Changed testConnection return type to boolean. Added throw clause which is thrown in case of connection errors

--- Abdul Weather Finder/src/meteo/http/Connectable.java
+++ Abdul Weather Finder/src/meteo/http/Connectable.java
@@ -1,5 +1,7 @@
 package meteo.http;
 
+import java.io.IOException;
+
 /**
  *
  * Connection interface for testing and proxy usage
@@ -12,6 +14,8 @@ public interface Connectable {
     public void applyProxy();
     /**
      * Sends a simple request to a link verifying connectivity
+     * @return - Connection success status
+     * @throws IOException - In case of connection error
      */
-    public void testConnection();
+    public boolean testConnection() throws IOException;
 }
