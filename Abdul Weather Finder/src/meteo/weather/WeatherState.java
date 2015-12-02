14292119997cb71314859b8fb16b9293628d036e
majid <majid@majid-Laptop>
2015-12-02 18:11:55 +0100

    Weather state holder class.

---
+++ Abdul Weather Finder/src/meteo/weather/WeatherState.java
@@ -0,0 +1,82 @@
+package meteo.weather;
+
+/**
+ *
+ * @author Abdul Majid <majid70111@hotmail.com>
+ */
+public class WeatherState {
+    private String ObservationTime;
+    private String TemperatureC;
+    private String WeatherHuman;
+    private String Humidity;
+    private String Feels;
+    private String WindKph;
+    private String WindDir;
+    private String WindMaxKph;
+    private String WindChill;
+    private String PressurePa;
+    private String VisibilityKm;
+    private String Uv;
+    private String RainToday;
+    private String icon;
+    
+    public WeatherState(){}
+
+    public void setObservationTime(String ObservationTime) {
+        this.ObservationTime = ObservationTime;
+    }
+
+    public void setTemperatureC(String TemperatureC) {
+        this.TemperatureC = TemperatureC;
+    }
+
+    public void setWeatherHuman(String WeatherHuman) {
+        this.WeatherHuman = WeatherHuman;
+    }
+
+    public void setHumidity(String Humidity) {
+        this.Humidity = Humidity;
+    }
+
+    public void setFeels(String Feels) {
+        this.Feels = Feels;
+    }
+
+    public void setWindKph(String WindKph) {
+        this.WindKph = WindKph;
+    }
+
+    public void setWindDir(String WindDir) {
+        this.WindDir = WindDir;
+    }
+
+    public void setWindMaxKph(String WindMaxKph) {
+        this.WindMaxKph = WindMaxKph;
+    }
+
+    public void setWindChill(String WindChill) {
+        this.WindChill = WindChill;
+    }
+
+    public void setPressurePa(String PressurePa) {
+        this.PressurePa = PressurePa;
+    }
+
+    public void setVisibilityKm(String VisibilityKm) {
+        this.VisibilityKm = VisibilityKm;
+    }
+
+    public void setUv(String Uv) {
+        this.Uv = Uv;
+    }
+
+    public void setRainToday(String RainToday) {
+        this.RainToday = RainToday;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+    
+    
+}
